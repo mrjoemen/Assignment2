990c7e9a678229f8da9dfbb5c5fe6268d647dad2
Jose Cabral <jose12marte@yahoo.com>
2020-11-09 13:28:52 -0500

    Started Chapter10

---
+++ src/Chapter10/MyInteger.java
@@ -0,0 +1,79 @@
+package Chapter10;
+
+public class MyInteger {
+
+    int value;
+
+    public MyInteger(int value) {
+        this.value = value;
+    }
+
+    public boolean isEven() {
+        return this.value % 2 == 0;
+    }
+
+    public boolean isOdd() {
+        return this.value % 2 == 1;
+    }
+
+    public boolean isPrime() {
+        for (int i = 2; i < this.value; i ++) {
+            if ( this.value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEven(MyInteger n) {
+        return n.value % 2 == 0;
+    }
+
+    public static boolean isOdd(MyInteger n) {
+        return n.value % 2 == 1;
+    }
+
+    public static boolean isPrime(MyInteger n) {
+        for (int i = 2; i < n.value; i ++) {
+            if (n.value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    public static boolean isOdd(int n) {
+        return n % 2 == 1;
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i ++) {
+            if (n% i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(MyInteger n) {
+        return this.value == n.value;
+    }
+
+    public boolean equals(int n) {
+        return this.value == n;
+    }
+
+    public static void parseInt(char[] arr) {
+        for (char c : arr) {
+            System.out.println((int) c);
+        }
+    }
+
+    public static int parseInt(String str) {
+        return str.charAt(0);
+    }
+}
---
+++ src/Chapter10/Time.java
@@ -0,0 +1,63 @@
+package Chapter10;
+import java.util.concurrent.TimeUnit;
+
+public class Time {
+
+    int hours;
+    int minutes;
+    int seconds;
+
+    public Time() {
+        this(System.currentTimeMillis());
+    }
+
+    public Time(long milliseconds) {
+        int hours = (int) ((milliseconds / 3600000) % 24);
+        int minutes = (int) ((milliseconds / 60000) % 60);
+        int seconds = (int) ((milliseconds / 1000) % 60);
+
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+
+    }
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s:%s:%s", this.hours, this.minutes, this.seconds);
+    }
+
+    public void setTime(long milliseconds) {
+        int hours = (int) ((milliseconds / 3600000) % 24);
+        int minutes = (int) ((milliseconds / 60000) % 60);
+        int seconds = (int) ((milliseconds / 1000) % 60);
+
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public static void main(String[] args) {
+        Time t1 = new Time(555550000);
+
+        System.out.println(t1.toString());
+    }
+}
